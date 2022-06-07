307b29f91658d45b450e19ece17bc9b8517568c8
Manoj R. Randeni <randeni.ramr@gmail.com>
2022-06-07 14:56:06 +0530

    create calculateAvailableCupCount() method

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,5 +1,8 @@
 package machine;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class CoffeeMachine {
@@ -14,7 +17,7 @@ public class CoffeeMachine {
 
     static int waterAmountPerCup = 200;
     static int milkAmountPerCup = 50;
-    static int coffeeBeansAmount = 15;
+    static int coffeeBeansAmountPerCup = 15;
 
     public static void main(String[] args) {
         startCoffeeMachine();
@@ -30,15 +33,27 @@ public class CoffeeMachine {
         System.out.println("Write how many grams of coffee beans the coffee machine has:");
         availableCoffeeBeansAmount = scanner.nextInt();
 
+        availableCupCount = calculateAvailableCupCount();
 
         System.out.println("Write how many cups of coffee you will need:");
         requestedCupCount = scanner.nextInt();
+
+
+    }
+
+    public static int calculateAvailableCupCount() {
+        List<Integer> ingredientsServingsCountList = new ArrayList();
+        ingredientsServingsCountList.set(0, availableWaterAmount / waterAmountPerCup);
+        ingredientsServingsCountList.set(1, availableMilkAmount / milkAmountPerCup);
+        ingredientsServingsCountList.set(2, availableCoffeeBeansAmount / coffeeBeansAmountPerCup);
+
+        return Collections.min(ingredientsServingsCountList);
     }
 
     public static void printTotalIngredientsAmount(int cups) {
         System.out.printf("For %d cups of coffee you will need:\n", cups);
         System.out.printf("%d ml of water\n", cups * waterAmountPerCup);
         System.out.printf("%d ml of milk\n", cups * milkAmountPerCup);
-        System.out.printf("%d g of coffee beans", cups * coffeeBeansAmount);
+        System.out.printf("%d g of coffee beans", cups * coffeeBeansAmountPerCup);
     }
 }
